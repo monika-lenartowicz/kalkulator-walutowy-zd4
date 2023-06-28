500d4864ace73ce36af4d705d720f2a52eeef89c
Monika Lenartowicz <nika.nel@interia.pl>
2023-06-28 16:10:00 +0200

    changes in path source

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ https://monika-lenartowicz.github.io/kalkulator-walutowy-zd4/
 
 ###Opis:
 
-Bardzo prosty przelicznik walut. 
+Bardzo prosty przelicznik walut.
 
-
+[![name]()](https://monika-lenartowicz.github.io/kalkulator-walutowy-zd4/images/currency_calculator.png)
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 {
-   const euro = document.querySelector(".js-euro");
-   const current = document.querySelector(".js-current");
-   const form = document.querySelector(".js-form");
-   const result = document.querySelector(".js-result");
+	const euro = document.querySelector(".js-euro");
+	const current = document.querySelector(".js-current");
+	const form = document.querySelector(".js-form");
+	const result = document.querySelector(".js-result");
 
-   form.addEventListener("submit", (event) => {
-      event.preventDefault();
+	const calculateResult = event => {
+		event.preventDefault();
 
-      const euroE = euro.value;
-      const currentE = current.value;
-      const resultE = euroE * currentE;
+		const euroE = euro.value;
+		const currentE = current.value;
+		const resultE = euroE * currentE;
 
-      result.innerText = resultE.toFixed(2);
-   })
+		result.innerText = resultE.toFixed(2);
+	};
 
+	form.addEventListener("submit", calculateResult);
 }
